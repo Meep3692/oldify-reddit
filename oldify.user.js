d5a175a776c0364f3e61ca10d67b6db90a2995df
Darian Smith <meep3692@gmail.com>
2024-10-05 02:05:08 -0400

    Fix new comments as they load

--- oldify.user.js
+++ oldify.user.js
@@ -36,10 +36,14 @@ function getQueryVariable(variable) {
     return "";
 }
 
+let postJson = null;
+
 function writeMD(mdtext, element){
+    console.log("Write " + mdtext + " to " + element);
     let md = marked.parse(mdtext);
     md = DOMPurify.sanitize(md);
     element.innerHTML = md;
+    element.setAttribute("oldify-fixed-md", true);
 }
 
 function getIdLimit(elem, rem){
@@ -49,19 +53,103 @@ function getIdLimit(elem, rem){
     return getIdLimit(elem.parentElement, rem - 1);
 }
 
-function getUsertext(json, mdElem){
-    let thingId = getIdLimit(mdElem, 10);
-    let [_, type, id] = thingId.split("_");
-    let list = [];
-    if(type == "t3") list = json[0].data.children;
-    else if(type == "t1") list = json[1].data.children;
-    else {
-        console.log("Unknown type for " + thingId);
-        return;
+let remaining = 0;
+
+let flat = {};
+let t1s = {};
+let t3s = {};
+
+function readJson(json){
+    for(let i = 0; i < json.length; i++){
+        let e = json[i];
+        if(e.data){
+            if(e.kind === "t1"){
+                t1s[e.data.id] = e;
+            }else if(e.kind === "t3"){
+                t3s[e.data.id] = e;
+            }
+            if(e.data && e.data.children){
+                let child = readJson(e.data.children);
+                if(child) return child;
+            }
+            if(e.data.replies){
+                let child = readJson(e.data.replies.data.children);
+                if(child) return child;
+            }
+        }
     }
-    let entry = list.find((e) => e.data.id == id);
+}
+
+function getFromJson(json, id){
+    console.log("List of length: " + json.length);
+    if(flat[id]) return flat[id];
+    for(let i = 0; i < json.length; i++){
+        let e = json[i];
+        if(e.data){
+            console.log("kind: " + e.kind + ", id: " + e.data.id);
+            if(e.data.id === id){
+                flat[id] = e;
+                return e;
+            }else{
+                if(e.data && e.data.children){
+                    let child = getFromJson(e.data.children, id);
+                    if(child) return child;
+                }
+                if(e.data.replies){
+                    let child = getFromJson(e.data.replies.data.children, id);
+                    if(child) return child;
+                }
+            }
+        }
+    }
+    return null;
+}
+
+async function commentUsertext(id){
+    remaining--;
+    if(remaining < 50){
+        console.log("Running low on rate limit!");
+        return false;
+    }
+    let commentUrl = window.location.href + id + ".json";
+    return await fetch(commentUrl).then(async (response) => {
+        if(!response.ok){
+            console.log("Couldn't get comment " + id);
+            console.log(response);
+        }
+        let json = await response.json();
+        readJson(json);
+        return json[1].data.children[0].data.body;
+    });
+}
+
+async function getUsertext(mdElem){
+    let thingId = getIdLimit(mdElem, 10);
+    console.log("Thing id: " + thingId);
+    let [_, type, id] = thingId.split("_");
+    if(!thingId){
+        console.log("Can't find id for " + mdElem);
+        return false;
+    }
+    // let list = [];
+    // if(type == "t3") list = postJson[0].data.children;
+    // else if(type == "t1") list = postJson[1].data.children;
+    // else {
+    //     console.log("Unknown type for " + thingId);
+    //     return;
+    // }
+    // let entry = list.find((e) => e.data.id == id);
+    //let entry = getFromJson(postJson, id);
+    let entry = null;
+    if(type == "t3") entry = t3s[id];
+    if(type == "t1") entry = t1s[id];
     if(!entry){
         console.log("Could not find entry for " + thingId);
+        if(type == "t1"){
+            console.log("Getting comment usertext from comment json");
+            return await commentUsertext(id);
+            //return false;
+        }
         return false;
     }
     let content = "";
@@ -71,7 +159,23 @@ function getUsertext(json, mdElem){
         console.log("Don't know how to find content for " + thingId);
         return false;
     }
+    console.log(content);
     return content;
+}
+
+async function fixMarkdown() {
+    [...document.getElementsByClassName("md")]
+        .filter((e) => !e.getAttribute("oldify-fixed-md"))
+        .map(async (e) => {return {elem: e, userText: await getUsertext(e)}})
+        .forEach(async (e) => {
+            e = await e;
+            console.log(e);
+            if(e.userText) writeMD(e.userText, e.elem)
+        });
+    // let elem = [...document.getElementsByClassName("md")]
+    //     .filter((e) => !e.getAttribute("oldify-fixed-md"))[3];
+    // let usertext = await getUsertext(elem);
+    // writeMD(usertext, elem);
 }
 
 const onNav = () => {
@@ -112,11 +216,12 @@ const onNav = () => {
                 console.log(response);
                 return;
             }
-            let json = await response.json();
-            [...document.getElementsByClassName("md")]
-                .map((e) => {return {elem: e, userText: getUsertext(json, e)}})
-                .filter((e) => e.userText)
-                .forEach((e) => writeMD(e.userText, e.elem));
+            let rateLimitRemaining = response.headers.get("x-ratelimit-remaining");
+            console.log(rateLimitRemaining);
+            remaining = rateLimitRemaining;
+            postJson = await response.json();
+            readJson(postJson);
+            fixMarkdown();
         });
         mutated();
     }
@@ -130,6 +235,9 @@ const mutated = () => {
         e.innerText = "";
         e.appendChild(img);
     });
+    if(postJson){
+        fixMarkdown();
+    }
 }
 
 (function() {
